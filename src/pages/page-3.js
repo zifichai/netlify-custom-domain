857aca39a2717fbb777b0d125acb65705476dc97
Bearcode Teeeps <bearcodeteeeps@gmail.com>
2019-07-29 21:13:19 -0500

    Second commit

--- src/pages/page-3.js
+++ src/pages/page-3.js
@@ -6,12 +6,12 @@ import SEO from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
+    <SEO title="Page three" />
+    <h1>Hi from the third page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
     <br></br>
-    <Link to="/page-3/">Go to page 3</Link>
+    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
